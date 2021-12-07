c5ff77adcc96787086001f2969dc833268b75187
kennedybabu <kennedynjogu8@gmail.com>
2021-12-07 12:10:59 +0300

    changes

--- news/views.py
+++ news/views.py
@@ -2,17 +2,32 @@ from django.core.checks import messages
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
-from .models import Article
+
+from news.forms import NewsLetterForm
+from .models import Article, NewsLetterRecipients
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .email import send_welcome_email
 
-# Create your views here.
-# def welcome(request):
-#     return render(request, 'welcome.html')
 
-def news_of_day(request):
+def news_today(request):
     date = dt.date.today()   
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date, "news": news})
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            email = form.cleaned_data['email']
+
+            recipient = NewsLetterRecipients(name = name, email = email)
+            recipient.save()
+            send_welcome_email(name, email)
+            
+            HttpResponseRedirect('news_today')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date, "news": news, "letterForm":form})
 
 def convert_dates(dates):
     day_number = dt.date.weekday(dates)
@@ -32,7 +47,7 @@ def past_days_news(request, past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
     news = Article.days_news(date)
     return render(request, 'all-news/past-news.html', {"date": date, "news": news})
@@ -58,3 +73,12 @@ def article(request, article_id):
     except ObjectDoesNotExist:
         raise Http404()
     return render(request, 'all-news/article.html', {"article":article})
+
+# def news_today(request):
+#     if request.method == 'POST':
+#         form = NewsLetterForm(request.POST)
+#         if form.is_valid():
+#             print('valid')
+#     else:
+#         form = NewsLetterForm()
+#     return render(request, 'all-news/today-news.html', {"date":date, })
